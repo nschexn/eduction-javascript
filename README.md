a5fc9c830b10f17deb49e23652eb0f06bffd2af8
Neal Schexnider <nschexn@hotmail.com>
2020-07-11 22:30:51 -0500

    api and duplicates

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Reference Notes for JavaScript
-
+---
 This repo is an area for me to quickly record and retrieve all notes related to JavaScript.
--- examples.ts
+++ examples.ts
@@ -3,4 +3,36 @@ function factorial(n){
   if(n === 1) return 1
   return n * factorial(n-1);
 }
-// console.log(factorial(4))
+
+// Promise
+const prom = () => new Promise((resolve, reject) => {
+  setTimeout(() => resolve(['a','b','c']), 
+  2000);
+})
+  .then(res => console.log(res));
+
+// ------- API ----------
+
+// Using fetch()
+/* 
+  fetch(url)
+    .then(function(data) {
+        // Here you get the data to modify as you please
+      })
+    })
+    .catch(function(error) {
+      // If there is any error you will catch them here
+    }); 
+*/
+
+
+const getBeers = () => fetch('https://api.punkapi.com/v2/beers')
+  .then(response => response.json())
+  .then(data => data)
+  .catch(error => error)
+
+console.log(getBeers)
+
+
+// filter or remove duplicates
+// nums.filter( (num, index) => nums.indexOf(num) === index);
